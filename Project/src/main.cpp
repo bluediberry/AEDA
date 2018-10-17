f561eacb659298fc9194e5e959b87b14e348cb5b
Mariana Neto <32868723+bluediberry@users.noreply.github.com>
2018-10-17 13:17:32 +0100

    main

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,11 +1,48 @@
+
+
 #include <iostream>
+using namespace std;
 
 int main() {
-    unsigned int menu=0;
-    int Clientes;
-
+    
+    int menu = 0;
+    int cliente;
+    
     while(menu != 4){
-
-
+        while(true){
+            cout << endl << "MENU INICIAL" << endl << endl;
+            cout << "[1] Informações" << endl;
+            cout << "[2] Registar" << endl;
+            cout << "[3] Reservar campo" << endl;
+            cout << "[4] Sair" << endl;
+            
+            cin >> menu;
+            
+            if(cin.fail()){
+                cin.clear();
+                cin.ignore('\n', 100);
+            }
+            
+            switch(menu){
+                case 1:
+                    //menuInformacao();
+                    break;
+                case 2:
+                    //Registar();
+                    break;
+                case 3:
+                    //Reservar();
+                    break;
+                case 4:
+                    exit(0);
+                default:
+                    return 0;
+            }
+            
+        }
+        
+        
     }
+    
+    return 0;
 }
